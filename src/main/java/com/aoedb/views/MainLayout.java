26fc0154f683fee80a11f15247e78c54a4882b32
start.vaadin.com <no-reply@vaadin.com>
2021-11-19 13:02:23 +0000

    Generated project

---
+++ src/main/java/com/aoedb/views/MainLayout.java
@@ -0,0 +1,185 @@
+package com.aoedb.views;
+
+import com.aoedb.views.buildings.BuildingsView;
+import com.aoedb.views.civilizations.CivilizationsView;
+import com.aoedb.views.gamemechanics.GameMechanicsView;
+import com.aoedb.views.mainmenu.MainMenuView;
+import com.aoedb.views.miscellaneous.MiscellaneousView;
+import com.aoedb.views.newquiz.NewQuizView;
+import com.aoedb.views.scores.ScoresView;
+import com.aoedb.views.technologies.TechnologiesView;
+import com.aoedb.views.techtree.TechTreeView;
+import com.aoedb.views.tools.ToolsView;
+import com.aoedb.views.units.UnitsView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.Nav;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.UnorderedList;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+@PWA(name = "AoE 2 Database", shortName = "AoE 2 Database", enableInstallPrompt = false)
+@Theme(themeFolder = "aoe2database")
+@PageTitle("Main")
+public class MainLayout extends AppLayout {
+
+    public static class MenuItemInfo {
+
+        private String text;
+        private String iconClass;
+        private Class<? extends Component> view;
+
+        public MenuItemInfo(String text, String iconClass, Class<? extends Component> view) {
+            this.text = text;
+            this.iconClass = iconClass;
+            this.view = view;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public String getIconClass() {
+            return iconClass;
+        }
+
+        public Class<? extends Component> getView() {
+            return view;
+        }
+
+    }
+
+    private H1 viewTitle;
+
+    public MainLayout() {
+        setPrimarySection(Section.DRAWER);
+        addToNavbar(true, createHeaderContent());
+        addToDrawer(createDrawerContent());
+    }
+
+    private Component createHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.addClassName("text-secondary");
+        toggle.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle = new H1();
+        viewTitle.addClassNames("m-0", "text-l");
+
+        Header header = new Header(toggle, viewTitle);
+        header.addClassNames("bg-base", "border-b", "border-contrast-10", "box-border", "flex", "h-xl", "items-center",
+                "w-full");
+        return header;
+    }
+
+    private Component createDrawerContent() {
+        H2 appName = new H2("AoE 2 Database");
+        appName.addClassNames("flex", "items-center", "h-xl", "m-0", "px-m", "text-m");
+
+        com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName,
+                createNavigation(), createFooter());
+        section.addClassNames("flex", "flex-col", "items-stretch", "max-h-full", "min-h-full");
+        return section;
+    }
+
+    private Nav createNavigation() {
+        Nav nav = new Nav();
+        nav.addClassNames("border-b", "border-contrast-10", "flex-grow", "overflow-auto");
+        nav.getElement().setAttribute("aria-labelledby", "views");
+
+        // Wrap the links in a list; improves accessibility
+        UnorderedList list = new UnorderedList();
+        list.addClassNames("list-none", "m-0", "p-0");
+        nav.add(list);
+
+        for (RouterLink link : createLinks()) {
+            ListItem item = new ListItem(link);
+            list.add(item);
+        }
+        return nav;
+    }
+
+    private List<RouterLink> createLinks() {
+        MenuItemInfo[] menuItems = new MenuItemInfo[]{ //
+                new MenuItemInfo("Main Menu", "la la-file", MainMenuView.class), //
+
+                new MenuItemInfo("Units", "la la-file", UnitsView.class), //
+
+                new MenuItemInfo("Buildings", "la la-file", BuildingsView.class), //
+
+                new MenuItemInfo("Technologies", "la la-file", TechnologiesView.class), //
+
+                new MenuItemInfo("Civilizations", "la la-file", CivilizationsView.class), //
+
+                new MenuItemInfo("Tech Tree", "la la-file", TechTreeView.class), //
+
+                new MenuItemInfo("Game Mechanics", "la la-file", GameMechanicsView.class), //
+
+                new MenuItemInfo("Tools", "la la-file", ToolsView.class), //
+
+                new MenuItemInfo("Miscellaneous", "la la-file", MiscellaneousView.class), //
+
+                new MenuItemInfo("New Quiz", "la la-file", NewQuizView.class), //
+
+                new MenuItemInfo("Scores", "la la-file", ScoresView.class), //
+
+        };
+        List<RouterLink> links = new ArrayList<>();
+        for (MenuItemInfo menuItemInfo : menuItems) {
+            links.add(createLink(menuItemInfo));
+
+        }
+        return links;
+    }
+
+    private static RouterLink createLink(MenuItemInfo menuItemInfo) {
+        RouterLink link = new RouterLink();
+        link.addClassNames("flex", "mx-s", "p-s", "relative", "text-secondary");
+        link.setRoute(menuItemInfo.getView());
+
+        Span icon = new Span();
+        icon.addClassNames("me-s", "text-l");
+        if (!menuItemInfo.getIconClass().isEmpty()) {
+            icon.addClassNames(menuItemInfo.getIconClass());
+        }
+
+        Span text = new Span(menuItemInfo.getText());
+        text.addClassNames("font-medium", "text-s");
+
+        link.add(icon, text);
+        return link;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+        layout.addClassNames("flex", "items-center", "my-s", "px-m", "py-xs");
+
+        return layout;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
---
+++ src/main/java/com/aoedb/views/techtree/TechTreeView.java
@@ -0,0 +1,31 @@
+package com.aoedb.views.techtree;
+
+import com.aoedb.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Tech Tree")
+@Route(value = "tech_tree", layout = MainLayout.class)
+public class TechTreeView extends VerticalLayout {
+
+    public TechTreeView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
