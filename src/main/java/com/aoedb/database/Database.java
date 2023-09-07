4d1b642e7b2e53fef4fee406e21194d3857de26c
pau795 <pau_795@hotmail.com>
2023-09-07 13:26:08 +0200

    Update 93001

--- src/main/java/com/aoedb/database/Database.java
+++ src/main/java/com/aoedb/database/Database.java
@@ -6,8 +6,8 @@ import java.util.*;
 public class Database {
 
     //DATABASE CONSTANTS
-    public final static String APP_VERSION = "v2.4";
-    public final static int PATCH_VERSION = 87863;
+    public final static String APP_VERSION = "v2.5";
+    public final static int PATCH_VERSION = 93001;
     public final static String BASE_DIR = "META-INF/resources/";
 
 
